cd0a5cb585d63ac9707f5a6c50915e6e5c718f3c
the-mogger <trapmesit@gmail.com>
2025-03-09 23:38:32 +0530

    Update gpttrain.py

--- gpttrain.py
+++ gpttrain.py
@@ -122,7 +122,7 @@ def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
     ax2.set_xlabel("Tokens seen")
 
     fig.tight_layout()  # Adjust layout to make room
-    # plt.show()
+    plt.show()
 
 
 def main(gpt_config, settings):
